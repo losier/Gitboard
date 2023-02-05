19900f29f0c5d69aef159784e3366bb4b0b89065
losier <arv95464@gmail.com>
2023-02-05 18:37:23 +0530

    basic setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
+import style from "../styles/Home.module.css";
+
+import imageURL from "../assets/github_icons/github_500.png";
 
 export default function Home() {
   return (
@@ -12,6 +16,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={style.main}>
+        <div className={style.container}>
+          <h1 className={style.title}>
+            Git <span>Board</span>
+          </h1>
+          <div className={style.imageContainer}>
+            <Image src={imageURL} alt="GitBoard" />
+          </div>
+          <div className={style.inputContainer}>
+            <input
+              type="text"
+              placeholder="Enter your github username"
+              className={style.input}
+            />
+          </div>
+        </div>
+      </main>
     </>
   );
 }
